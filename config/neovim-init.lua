b84d9a9f8b03ed1b304edbd33feb43afb2aecb9e
Miere Teixeira <miere00@gmail.com>
2026-04-07 15:28:26 +1000

    Small tweaks on the includes and workflows

---
+++ config/neovim-init.lua
@@ -0,0 +1,25 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", lazypath
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local ok, configs = pcall(require, "nvim-treesitter.configs")
+      if not ok then return end
+      configs.setup({
+        ensure_installed = { "lua", "python", "javascript" },
+        highlight = { enable = true },
+      })
+    end,
+  },
+})
+
